ec5434d05814f824c06c4fe6c64e83651abac7fe
MOZARTA <mcukeli@uet.edu.al>
2026-05-29 11:23:18 +0200

    updated BACKEND

--- BACKEND/routes/taskRoutes.js
+++ BACKEND/routes/taskRoutes.js
@@ -2,12 +2,11 @@ const express = require('express');
 	const router = express.Router();
 
    const { getTasks } = require("../controllers/taskControllers");
+	const { protect } = require('../middleware/authMiddleware');
+
+   router.get("/", protect,  getTasks);
+    router.post("/", protect, setTask);
+	router.put("/:id", protect,  updateTask);
+	router.delete("/:id", protect,  deleteTask);
 	
-   router.get("/", getTasks);
-    router.post("/", setTask);
-	router.put("/:id", updateTask);
-	router.delete("/:id", deleteTask);
-
-
-
 	module.exports = router;
